942aafd294b4a120746e5305815663997e14da0a
Robert Kaye <rob@musicbrainz.org>
2020-07-04 15:35:27 +0200

    Finish entities tests

--- troi/tests/test_entities.py
+++ troi/tests/test_entities.py
@@ -4,7 +4,7 @@ import unittest
 from troi import Artist, Release, Recording
 
 
-class TestOperations(unittest.TestCase):
+class TestEntities(unittest.TestCase):
 
     def test_artist(self):
 
@@ -22,6 +22,36 @@ class TestOperations(unittest.TestCase):
         assert a.mbids[1] == "afe3f1d4-b6d5-4726-89ad-926e3420b9e3"
 
         a = Artist(listenbrainz={ 1 : 2}, musicbrainz={ 3 : 4}, acousticbrainz={ 5 : 6})
+        assert a.lb[1] == 2
+        assert a.mb[3] == 4
+        assert a.ab[5] == 6
+        assert a.name == None
+        assert a.mbid == None
+        assert a.msid == None
+
+    def test_release(self):
+
+        a = Release("Dummy", "8fe3f1d4-b6d5-4726-89ad-926e3420b9e3", "97b01626-65fc-4c32-b30c-c4d7eab1b339")
+        assert a.name == "Dummy"
+        assert a.mbid == "8fe3f1d4-b6d5-4726-89ad-926e3420b9e3"
+        assert a.msid == "97b01626-65fc-4c32-b30c-c4d7eab1b339"
+
+        a = Release(listenbrainz={ 1 : 2}, musicbrainz={ 3 : 4}, acousticbrainz={ 5 : 6})
+        assert a.lb[1] == 2
+        assert a.mb[3] == 4
+        assert a.ab[5] == 6
+        assert a.name == None
+        assert a.mbid == None
+        assert a.msid == None
+
+    def test_recording(self):
+
+        a = Recording("Strangers", "8fe3f1d4-b6d5-4726-89ad-926e3420b9e3", "97b01626-65fc-4c32-b30c-c4d7eab1b339")
+        assert a.name == "Strangers"
+        assert a.mbid == "8fe3f1d4-b6d5-4726-89ad-926e3420b9e3"
+        assert a.msid == "97b01626-65fc-4c32-b30c-c4d7eab1b339"
+
+        a = Recording(listenbrainz={ 1 : 2}, musicbrainz={ 3 : 4}, acousticbrainz={ 5 : 6})
         assert a.lb[1] == 2
         assert a.mb[3] == 4
         assert a.ab[5] == 6
