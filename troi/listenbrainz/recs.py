a9154c9b13099ce0cca0acd00ab0f21c752ff8ae
Robert Kaye <rob@musicbrainz.org>
2020-10-07 16:55:03 +0200

    Fetch more than 100 recommendations, if available

--- troi/listenbrainz/recs.py
+++ troi/listenbrainz/recs.py
@@ -11,6 +11,9 @@ class UserRecordingRecommendationsElement(Element):
         Fetch recommended recording for a user from ListenBrainz
     '''
 
+    MAX_RECORDINGS_PER_REQUEST = 100
+    MAX_RECORDINGS = 200
+
     def __init__(self, user_name, artist_type, count=25, offset=0):
         """
             The offset parameter does not work yet.
@@ -32,11 +35,23 @@ class UserRecordingRecommendationsElement(Element):
 
     def read(self, inputs = []):
         recording_list = []
-        recordings = self.client.get_user_recommendation_recordings(self.user_name, self.artist_type, self.count, self.offset)
-        self._last_updated = datetime.datetime.fromtimestamp(recordings['payload']['last_updated'])
+        recordings = []
 
-        artist_type = self.artist_type + "_artist"  
-        for r in recordings['payload']['mbids']:
-            recording_list.append(Recording(mbid=r['recording_mbid'], listenbrainz={'score':r['score']}))
+        while True:
+            recordings = self.client.get_user_recommendation_recordings(self.user_name, 
+                                                                        self.artist_type, 
+                                                                        count=self.count - len(recording_list),
+                                                                        offset=len(recording_list))
+
+            if not len(recordings['payload']['mbids']):
+                break
+
+            for r in recordings['payload']['mbids']:
+                recording_list.append(Recording(mbid=r['recording_mbid'], listenbrainz={'score':r['score']}))
+
+            if len(recording_list) >= self.count:
+                break
+
+        self._last_updated = datetime.datetime.fromtimestamp(recordings['payload']['last_updated'])
 
         return recording_list
--- troi/musicbrainz/recording_lookup.py
+++ troi/musicbrainz/recording_lookup.py
@@ -16,7 +16,7 @@ class RecordingLookupElement(Element):
         Look up a musicbrainz data for a list of recordings, based on MBID. 
     '''
 
-    SERVER_URL = "http://bono.metabrainz.org:8000/recording-mbid-lookup/json"
+    SERVER_URL = "https://labs.api.listenbrainz.org/recording-mbid-lookup/json"
 
     def inputs(self):
         return [ Recording ]
@@ -33,7 +33,7 @@ class RecordingLookupElement(Element):
         data = []
         r_mbids = ",".join([ r.mbid for r in recordings ])
         for r in recordings:
-            data.append({ 'recording_mbid': r.mbid })
+            data.append({ '[recording_mbid]': r.mbid })
         r = requests.post(self.SERVER_URL, json=data)
         if r.status_code != 200:
             r.raise_for_status()
@@ -45,7 +45,7 @@ class RecordingLookupElement(Element):
 
         mbid_index = {}
         for row in rows:
-            mbid_index[row['recording_mbid']] = row
+            mbid_index[row['original_recording_mbid']] = row
 
         for r in recordings:
             row = mbid_index[r.mbid]
