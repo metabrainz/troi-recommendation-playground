7a8242ad97ba0e16ff0546f58953bc1fce42e987
Robert Kaye <rob@musicbrainz.org>
2020-10-13 17:29:13 +0200

    Started on an artist limiter

--- troi/filters.py
+++ troi/filters.py
@@ -51,3 +51,53 @@ class ArtistCreditFilterElement(troi.Element):
                     results.append(r)
 
         return results
+
+
+class ArtistCreditLimiterElement(troi.Element):
+
+    def __init__(self, limit=2, exclude_lower_ranked=True):
+        '''
+            This element examines there passed in recordings and if the count of
+            recordings by any one artists exceeds the given limit, excessive recordigns
+            are removed. If the flag exclude_lower_ranked is True, then the lowest
+            ranked recordings are removed, otherwise the highest ranked recordings
+            are removed. Throws RuntimeError is not all recordings have
+            artist_credit_ids set.
+        '''
+        self.limit = limit
+        self.exclude_lower_ranked = exclude_lower_ranked
+
+    @staticmethod
+    def inputs():
+        return [Recording]
+
+    @staticmethod
+    def outputs():
+        return [Recording]
+
+    def read(self, inputs, debug=False):
+
+        recordings = inputs[0]
+
+        ac_index = {}
+        for ac in self.artist_credit_ids:
+            try:
+                ac_index[ac] = 1
+            except KeyError:
+                raise RuntimeError(self.__name__ + " needs to have all input recordings to have ac.artist_credit_id defined!")
+
+        results = []
+        for r in recordings:
+            if not r.artist or not r.artist.artist_credit_id:
+                if debug:
+                    print("- debug recording %s has not artist credit id" % (r.mbid))
+                continue
+
+            if self.include:
+                if r.artist.artist_credit_id in ac_index:
+                    results.append(r)
+            else:
+                if r.artist.artist_credit_id not in ac_index:
+                    results.append(r)
+
+        return results
