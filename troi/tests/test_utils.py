b0c8a1bf07cd3eb5d206e83db2c941e91f7b3038
Robert Kaye <rob@musicbrainz.org>
2020-10-23 16:26:47 +0200

    Start a test loading patch

---
+++ troi/tests/test_utils.py
@@ -0,0 +1,18 @@
+import unittest
+
+from troi.utils import discover_patches
+from troi.patch import Patch
+
+
+class TestPatches(unittest.TestCase):
+
+    def test_discover_patches(self):
+        patches = discover_patches()
+
+        assert len(patches) == 2
+        assert "daily-jams" in patches
+        assert "area-random-recordings" in patches
+
+        print(patches)
+        assert issubclass(patches['daily-jams'], Patch)
+        assert issubclass(patches['area-random-recordings'], Patch)
