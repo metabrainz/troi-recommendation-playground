ad97e2070faae250c3a7a7c6699cd6cb565e09cd
Robert Kaye <rob@musicbrainz.org>
2020-11-11 17:14:43 +0100

    add test for year element.

---
+++ troi/musicbrainz/tests/test_year_lookup.py
@@ -0,0 +1,48 @@
+import json
+import unittest
+import unittest.mock
+
+import troi
+import troi.musicbrainz.year_lookup
+
+return_json = [
+    {
+        "artist_credit_name": "morcheeba",
+        "recording_name": "trigger hippie",
+        "year": 1997
+    }
+]
+
+class TestYearLookup(unittest.TestCase):
+
+    @unittest.mock.patch('requests.post')
+    def test_read(self, req):
+
+        mock = unittest.mock.MagicMock()
+        mock.status_code = 200
+        mock.text = json.dumps(return_json)
+        req.return_value = mock
+        e = troi.musicbrainz.year_lookup.YearLookupElement()
+
+        r = [ troi.Recording("trigger hippie", artist=troi.Artist("morcheeba")) ]
+        entities = e.read([r])
+        req.assert_called_with(e.SERVER_URL, json=[{ '[recording_name]': 'trigger hippie', '[artist_credit_name]': 'morcheeba' }])
+
+        assert len(entities) == 1
+        assert entities[0].artist.name == "morcheeba"
+        assert entities[0].name == "trigger hippie"
+        assert entities[0].year == 1997
+
+
+    @unittest.mock.patch('requests.get')
+    def test_read_remove_unmatched(self, req):
+
+        mock = unittest.mock.MagicMock()
+        mock.status_code = 200
+        mock.text = "{}"
+        req.return_value = mock
+        e = troi.musicbrainz.year_lookup.YearLookupElement()
+
+        r = [ troi.Recording("track not found", artist=troi.Artist("artist not found")) ]
+        entities = e.read([r])
+        assert len(entities) == 0
