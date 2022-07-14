2891d1994706101f151a3201b111ea083bfcb0f8
Kartik Ohri <kartikohri13@gmail.com>
2022-07-14 23:45:39 +0530

    Generate install_requires from requirements.txt
    When installing troi as a library, pip will look at install_requires and
    not requirements.txt (which is only used during local development). Often,
    the two diverge because we forget to update in 1 place. So generate
    install_requires from requirements.txt.

--- setup.py
+++ setup.py
@@ -3,6 +3,11 @@ from setuptools import setup, find_packages
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+# read dependencies from requirements.txt skipping any comment lines
+with open("requirements.txt", "r") as fh:
+    requirements = [line for line in fh.read().splitlines() if not line.startswith("#")]
+
+
 setup(
     name="troi",
     version="0.1.0",
@@ -11,7 +16,7 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/metabrainz/troi-recommendation-playground",
-    install_requires=["click>=7", "ujson==5.4.0", "requests>=2.24", "pylistenbrainz", "openpost"],
+    install_requires=requirements,
     packages=find_packages(exclude=("patches",)),
     classifiers=[
         "Programming Language :: Python :: 3",
