ecefd2bc0ab488bcced498aeb0d2e9813c32cf58
Robert Kaye <rob@musicbrainz.org>
2022-07-21 13:58:04 +0200

    Use metadata lookup to lookup genres now.

--- troi/musicbrainz/genre_lookup.py
+++ troi/musicbrainz/genre_lookup.py
@@ -1,7 +1,9 @@
 import requests
-import ujson
+import json
 
-from troi import Element, Recording, PipelineError, DEVELOPMENT_SERVER_URL
+from troi import Element, Recording, PipelineError
+
+MAX_MBIDS_PER_CALL = 20
 
 
 class GenreLookupElement(Element):
@@ -9,10 +11,12 @@ class GenreLookupElement(Element):
         Look up musicbrainz tag and genres for a list of recordings recordings, based on recording mbid.
     """
 
-    SERVER_URL = DEVELOPMENT_SERVER_URL + "/genre-mbid-lookup/json"
+    SERVER_URL = "https://api.listenbrainz.org/1/metadata/recording"
+#/?recording_mbids=e97f805a-ab48-4c52-855e-07049142113d&inc=tag
 
-    def __init__(self):
+    def __init__(self, count_threshold=3):
         Element.__init__(self)
+        self.count_threshold = count_threshold
 
     @staticmethod
     def inputs():
@@ -28,39 +32,46 @@ class GenreLookupElement(Element):
         if not recordings:
             return []
 
-        data = []
+        mbid_sets = []
+        mbids = []
         for r in recordings:
-            data.append({'[recording_mbid]': r.mbid})
-
-        r = requests.post(self.SERVER_URL, json=data)
-        if r.status_code != 200:
-            raise PipelineError("Cannot fetch recording tags from MusicBrainz: HTTP code %d" % r.status_code)
-
-        try:
-            rows = ujson.loads(r.text)
-        except ValueError as err:
-            raise PipelineError("Cannot fetch recording tags from MusicBrainz: " + str(err))
-
-        mbid_index = {}
-        for row in rows:
-            mbid_index[row['recording_mbid']] = row
+            mbids.append(r.mbid)
+            if len(mbids) >= MAX_MBIDS_PER_CALL:
+                mbid_sets.append(mbids)
+                mbids = []
 
         output = []
+
+        data = {}
+        for mbid_set in mbid_sets:
+            r = requests.get(self.SERVER_URL, params={ "recording_mbids" : ",".join(mbid_set), "inc": "tag" })
+            if r.status_code != 200:
+                raise PipelineError("Cannot fetch tags from ListenBrainz: HTTP code %d" % r.status_code)
+
+            data = {**data, **r.json()}
+
         for r in recordings:
-            try:
-                tags = mbid_index[r.mbid].get('tags', '')
-                if tags:
-                    r.musicbrainz['tags'] = tags.split(',')
-                else:
-                    r.musicbrainz['tags'] = []
-                genres = mbid_index[r.mbid].get('genres', '')
-                if genres:
-                    r.musicbrainz['genres'] = genres.split(',')
-                else:
-                    r.musicbrainz['genres'] = []
-            except KeyError:
-                self.debug("recording (%s) not found, skipping." % r.mbid)
+
+            if r.mbid not in data:
                 continue
+
+            # Save the whole MB metadata tag info
+            r.musicbrainz["tag_metadata"] = data[r.mbid]["tag"]
+
+
+            genres = []
+            for genre in data[r.mbid]["tag"]["recording"]:
+                if genre["count"] >= self.count_threshold:
+                    genres.append(genre["tag"])
+
+            r.musicbrainz["genres"] = genres
+
+            artist_genres = []
+            for genre in data[r.mbid]["tag"]["artist"]:
+                if genre["count"] >= self.count_threshold:
+                    artist_genres.append(genre["tag"])
+
+            r.artist.musicbrainz["genres"] = artist_genres
 
             output.append(r)
 
