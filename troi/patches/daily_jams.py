8a7acf0525f6285c70d4efd26daa14ea7b593572
Kartik Ohri <kartikohri13@gmail.com>
2022-10-17 16:28:30 +0530

    Add DAYS_OF_RECENT_LISTENS_TO_EXCLUDE constant

--- troi/patches/daily_jams.py
+++ troi/patches/daily_jams.py
@@ -15,6 +15,9 @@ def cli():
     pass
 
 
+DAYS_OF_RECENT_LISTENS_TO_EXCLUDE = 14  # Exclude tracks listened in last X days from the daily jams playlist
+
+
 class DailyJamsPatch(troi.patch.Patch):
     """
         Taken a list of Recordings, break them into 7 roughly equal chunks and return
@@ -64,7 +67,7 @@ class DailyJamsPatch(troi.patch.Patch):
         recent_listens_lookup = troi.listenbrainz.listens.RecentListensTimestampLookup(user_name, days=2)
         recent_listens_lookup.set_sources(raw_recs_lookup)
 
-        latest_filter = troi.filters.LatestListenedAtFilterElement(14)
+        latest_filter = troi.filters.LatestListenedAtFilterElement(DAYS_OF_RECENT_LISTENS_TO_EXCLUDE)
         latest_filter.set_sources(recent_listens_lookup)
 
         pl_maker = PlaylistMakerElement(name="Daily Jams for %s, %s" % (user_name, jam_date),
