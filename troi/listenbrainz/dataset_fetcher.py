83f9d77971684a40f1776e9e7464f5148a28a730
Robert Kaye <rob@musicbrainz.org>
2023-01-30 16:46:20 +0100

    Minor tweaks

--- troi/listenbrainz/dataset_fetcher.py
+++ troi/listenbrainz/dataset_fetcher.py
@@ -14,10 +14,11 @@ class DataSetFetcherElement(Element):
             json_post_data: the dict data that will be POSTed as JSON
     '''
 
-    def __init__(self, server_url, json_post_data):
+    def __init__(self, server_url, json_post_data, max_num_items=None):
         super().__init__()
         self.server_url = server_url
         self.json_post_data = json_post_data
+        self.max_num_items = max_num_items
 
     @staticmethod
     def inputs():
@@ -27,10 +28,9 @@ class DataSetFetcherElement(Element):
     def outputs():
         return [Recording, Artist]
 
-
     def create_recording(self, row):
 
-        r = Recording(mbid=row['recording_mbid']) 
+        r = Recording(mbid=row['recording_mbid'])
         if 'artist_credit_name' in row:
             if 'artist_credit_id' in row:
                 r.artist.artist_credit_id = row['artist_credit_id']
@@ -60,47 +60,53 @@ class DataSetFetcherElement(Element):
             r.year = row['year']
 
         if 'listen_count' in row:
-            r.listenbrainz={"listen_count": row["listen_count"]}
+            r.listenbrainz = {"listen_count": row["listen_count"]}
 
         if 'bpm' in row:
-            r.acousticbrainz={"bpm": row["bpm"]}
+            r.acousticbrainz = {"bpm": row["bpm"]}
 
         return r
 
-    def create_artists(self, row):
-        # TODO REview this, not finished!
+    def create_artist(self, row):
 
-        artist = None
+        if 'artist_mbid' in row:
+            return Artist(mbids=[row['artist_mbid']])
+
         if 'artist_credit_name' in row:
-            artist = Artist(name=row['artist_credit_name'])
+            return Artist(name=row['artist_credit_name'])
 
         if 'artist_credit_id' in row:
-            if artist is None:
-                artist = Artist(artist_credit_id=row['artist_credit_id'])
-            else:
-                artist.artist_credit_id = row['artist_credit_id']
+            return Artist(artist_credit_id=row['artist_credit_id'])
 
         if 'artist_mbids' in row:
-            if artist is None:
-                artist = Artist(mbids=row['artist_mbids'])
-            else:
-                artist.mbids = row['artist_mbids']
+            return Artist(mbids=row['artist_mbids'])
 
-        return artist
+        return None
 
     def read(self, inputs):
 
         r = requests.post(self.server_url, json=self.json_post_data)
         if r.status_code != 200:
-            raise PipelineError("Cannot fetch first dataset recordings from ListenBrainz. HTTP code %s (%s)" % (r.status_code, r.text))
+            raise PipelineError("Cannot fetch first dataset recordings from ListenBrainz. HTTP code %s (%s)" %
+                                (r.status_code, r.text))
+
+        try:
+            data = r.json()[3]["data"]
+        except (IndexErrror, KeyError):
+            data = r.json()
 
         output = []
-        for row in r.json():
+        for row in data:
             if "recordind_mbid" in row:
                 r = self.create_recording(row)
             elif "artist_mbid" in row:
                 r = self.create_artist(row)
+            else:
+                continue
 
             output.append(r)
+
+        if self.max_num_items is not None:
+            output = output[:self.max_num_items]
 
         return output
--- troi/patches/artist_radio.py
+++ troi/patches/artist_radio.py
@@ -1,11 +1,13 @@
 from datetime import datetime
 
+import requests
+
 import troi.filters
 import troi.listenbrainz.feedback
 import troi.listenbrainz.listens
 import troi.listenbrainz.recs
 import troi.musicbrainz.recording_lookup
-from troi import Playlist, Element, Recording
+from troi import Playlist, Element, Recording, Artist, PipelineError
 from troi.playlist import PlaylistMakerElement
 from troi.listenbrainz.dataset_fetcher import DataSetFetcherElement
 
@@ -18,6 +20,9 @@ class UnionElement(troi.Element):
     def inputs(self):
         return []
 
+    def outputs(self):
+        return []
+
     def read(self, entities):
 
         recordings = []
@@ -25,6 +30,38 @@ class UnionElement(troi.Element):
             recordings.extend(entity)
 
         return recordings
+
+
+class PopularRecordingsElement(troi.Element):
+
+    server_url = "https://datasets.listenbrainz.org/popular-recordings/json"
+
+    def __init__(self, max_num_recordings=3):
+        troi.Element.__init__(self)
+        self.max_num_recordings = max_num_recordings
+
+    def inputs(self):
+        return [Artist]
+
+    def outputs(self):
+        return [Recording]
+
+    def read(self, entities):
+
+        for a in entities[0]:
+            print(a.mbids)
+
+        r = requests.post(self.server_url, json=[ { "[artist_mbid]" : a.mbids[0] } for a in entities[0] ])
+        if r.status_code != 200:
+            raise PipelineError("Cannot fetch popular recordings from ListenBrainz. HTTP code %s (%s)" % (r.status_code, r.text))
+
+        output = []
+        for row in r.json():
+            output.append(Recording(mbid=row["recording_mbid"]))
+            if len(output) >= self.max_num_recordings:
+                break
+
+        return output
 
 
 class ArtistRadioPatch(troi.patch.Patch):
@@ -62,8 +99,8 @@ class ArtistRadioPatch(troi.patch.Patch):
 
         elements = []
         for artist_mbid in artist_mbids:
-            print(f'{artist_mbid}')
             elements.append(DataSetFetcherElement(server_url="https://labs.api.listenbrainz.org/similar-artists/json",
+                                     max_num_items=5,
                                      json_post_data=[{
                                          'artist_mbid': artist_mbid,
                                          'algorithm': "session_based_days_1800_session_300_contribution_3_threshold_10_limit_100_filter_True_skip_30"
@@ -72,8 +109,11 @@ class ArtistRadioPatch(troi.patch.Patch):
         union = UnionElement()
         union.set_sources(elements)
 
+        pop_recordings = PopularRecordingsElement(max_num_recordings=3)
+        pop_recordings.set_sources(union)
+
         recs_lookup = troi.musicbrainz.recording_lookup.RecordingLookupElement()
-        recs_lookup.set_sources(union)
+        recs_lookup.set_sources(pop_recordings)
 
         pl_maker = PlaylistMakerElement(name="Artist Radio for %s" % (",".join(artist_mbids)),
                                         desc="Experimental artist radio playlist",
