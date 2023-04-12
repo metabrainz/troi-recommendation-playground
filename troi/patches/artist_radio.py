1cffca74272205623347a48df370adc3e49a44ac
Robert Kaye <rob@musicbrainz.org>
2023-04-12 16:41:01 +0200

    Minor progress towards better weighted artists and recordings

--- troi/patches/artist_radio.py
+++ troi/patches/artist_radio.py
@@ -57,11 +57,14 @@ class InterleaveRecordingsElement(troi.Element):
 class ArtistRadioSourceElement(troi.Element):
 
     MAX_NUM_SIMILAR_ARTISTS = 10
+    MAX_TOP_RECORDINGS_PER_ARTIST = 15
+    KEEP_TOP_RECORDINGS_PER_ARTIST = 100
 
-    def __init__(self, artist_mbid):
+    def __init__(self, artist_mbid, mode="easy"):
         troi.Element.__init__(self)
         self.artist_mbid = artist_mbid
         self.similar_artists = []
+        self.mode = mode
 
     def inputs(self):
         return []
@@ -75,7 +78,6 @@ class ArtistRadioSourceElement(troi.Element):
             '[artist_mbid]': artist_mbid,
         }])
         return r.json()
-
 
     def get_similar_artists(self, artist_mbid):
 
@@ -105,29 +107,60 @@ class ArtistRadioSourceElement(troi.Element):
 
         # Start collecting data
         self.similar_artists = []
-        dss = DataSetSplitter(similar_artist_data, 3)
-        for similar_artist in dss[0] + dss[1]:
-            print("Fetch similar: ", similar_artist["artist_mbid"])
+        dss = DataSetSplitter(similar_artist_data, 4)
+
+        if self.mode == "easy":
+            similar_artists_filtered = dss[0] + dss[1]
+        elif self.mode == "medium":
+            similar_artists_filtered = dss[1] + dss[2]
+        else:
+            similar_artists_filtered = dss[2] + dss[3]
+
+        for similar_artist in similar_artists_filtered:
             recordings = self.fetch_top_recordings(similar_artist["artist_mbid"])
-            self.similar_artists.append({ "artist_mbid": similar_artist["artist_mbid"], "recordings": recordings[:15] })
+            if len(recordings) == 0:
+                continue
+
+            # Keep only a certain number of top recordings
+            recordings = recordings[:self.KEEP_TOP_RECORDINGS_PER_ARTIST]
+
+            self.similar_artists.append({
+                "artist_mbid": similar_artist["artist_mbid"],
+                "artist_name": similar_artist["name"],
+                "raw_score": similar_artist["score"],
+                "recordings": recordings,
+                "dss": DataSetSplitter(recordings, 4, field="count")
+            })
 
             if len(self.similar_artists) >= self.MAX_NUM_SIMILAR_ARTISTS:
                 break
 
+        # Normalize similar artist scores
+        max_score = 0
+        for sim in self.similar_artists:
+            max_score = max(max_score, sim["raw_score"])
+
+        for sim in self.similar_artists:
+            sim["score"] = sim["raw_score"] / float(max_score)
+
+            # And also normalize recording scores
+            max_count = 0
+            for rec in sim["recordings"]:
+                max_count = max(max_count, rec["count"])
+
+            for rec in sim["recordings"]:
+                rec["count"] = rec["count"] / float(max_count)
+
+            print("  similar: %.3f %d %s" % (sim["score"], len(sim["recordings"]), sim["artist_name"]))
 
         # Now that data is collected, collate tracks into one single list
         recs = []
         print("Collate")
-        while True:
-            empty = 0
-            for similar_artist in self.similar_artists:
-                try:
-                    recs.append(Recording(mbid=similar_artist["recordings"].pop(0)["recording_mbid"]))
-                except IndexError:
-                    empty += 1
+        for similar_artist in self.similar_artists:
+            for rec in similar_artist["recordings"][:self.MAX_TOP_RECORDINGS_PER_ARTIST]:
+                recs.append(Recording(mbid=rec["recording_mbid"]))
 
-            if empty == len(self.similar_artists):
-                break
+        shuffle(recs)
 
         return recs
 
@@ -146,9 +179,24 @@ class ArtistRadioPatch(troi.patch.Patch):
         Generate a playlist from one or more Artist MBIDs
 
         \b
+        MODE which mode to generate playlists in. must be one of easy, mediumedium, hard
         ARTIST_MBIDs is a list of artist_mbids to be used as seeds
         """
-        return [{"type": "argument", "args": ["artist_mbid"], "kwargs": {"required": False, "nargs": -1}}]
+        return [{
+            "type": "argument",
+            "args": ["mode"],
+            "kwargs": {
+                "required": True,
+                "nargs": 1
+            }
+        }, {
+            "type": "argument",
+            "args": ["artist_mbid"],
+            "kwargs": {
+                "required": False,
+                "nargs": -1
+            }
+        }]
 
     @staticmethod
     def outputs():
@@ -163,18 +211,21 @@ class ArtistRadioPatch(troi.patch.Patch):
         return "Given one or more artist_mbids, return a list playlist of those and similar artists."
 
     def create(self, inputs):
-        artist_mbids = inputs['artist_mbid']
+        artist_mbids = inputs["artist_mbid"]
+        mode = inputs["mode"]
+        print(mode)
 
+        if mode not in ("easy", "medium", "hard"):
+            raise RuntimeError("Argument mode must be one one easy, medium or hard.")
 
         lookups = []
         for mbid in artist_mbids:
-            ar_source = ArtistRadioSourceElement(mbid)
+            ar_source = ArtistRadioSourceElement(mbid, mode)
 
             recs_lookup = troi.musicbrainz.recording_lookup.RecordingLookupElement()
             recs_lookup.set_sources(ar_source)
 
             lookups.append(recs_lookup)
-
 
         interleave = InterleaveRecordingsElement()
         interleave.set_sources(lookups)
@@ -183,8 +234,7 @@ class ArtistRadioPatch(troi.patch.Patch):
                                         desc="Experimental artist radio playlist",
                                         patch_slug=self.slug(),
                                         max_num_recordings=50,
-                                        max_artist_occurrence=5,
-                                        shuffle=True)
+                                        max_artist_occurrence=5)
         pl_maker.set_sources(interleave)
 
         return pl_maker
