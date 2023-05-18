0a910274402a0111d74f0bc07d27935f6f98fb1e
Robert Kaye <rob@musicbrainz.org>
2023-05-18 10:42:00 +0200

    Saved playlist URLs

--- troi/playlist.py
+++ troi/playlist.py
@@ -122,6 +122,7 @@ class PlaylistElement(Element):
         super().__init__()
         self.playlists = []
         self.print_recording = PrintRecordingList()
+        self.playlist_urls = []
 
     @staticmethod
     def inputs():
@@ -199,7 +200,7 @@ class PlaylistElement(Element):
         if not self.playlists:
             raise PipelineError("Playlists have not been generated yet.")
 
-        playlist_mbids = []
+        self.playlist_urls = []
         for playlist in self.playlists:
             if len(playlist.recordings) == 0:
                 continue
@@ -224,9 +225,9 @@ class PlaylistElement(Element):
                 raise PipelineError("Cannot post playlist to ListenBrainz: " + str(err))
 
             playlist.mbid = result["playlist_mbid"]
-            playlist_mbids.append((LISTENBRAINZ_SERVER_URL + "/playlist/" + result["playlist_mbid"], result["playlist_mbid"]))
+            self.playlist_urls.append((LISTENBRAINZ_SERVER_URL + "/playlist/" + result["playlist_mbid"], result["playlist_mbid"]))
 
-        return playlist_mbids
+        return self.playlist_urls
 
     def submit_to_spotify(self,
                           user_id: str,
