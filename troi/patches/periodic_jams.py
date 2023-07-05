cb01f34139590d4edd8d0da36a00520daa725364
Kartik Ohri <kartikohri13@gmail.com>
2023-07-05 21:22:26 +0530

    Separate spotify submission into a utility function (#104)
    So that we can submit bulk generated playlists from spark without creating
    unnecessary patch objects.

--- troi/patches/periodic_jams.py
+++ troi/patches/periodic_jams.py
@@ -5,9 +5,8 @@ import troi.listenbrainz.feedback
 import troi.listenbrainz.listens
 import troi.listenbrainz.recs
 import troi.musicbrainz.recording_lookup
-from troi import Playlist, Element, Recording
-from troi.musicbrainz.recording import RecordingListElement
-from troi.playlist import PlaylistMakerElement, PlaylistShuffleElement
+from troi import Playlist
+from troi.playlist import PlaylistMakerElement
 
 DAYS_OF_RECENT_LISTENS_TO_EXCLUDE = 60  # Exclude tracks listened in last X days from the daily jams playlist
 DAILY_JAMS_MIN_RECORDINGS = 25  # the minimum number of recordings we aspire to have in a daily jam, this is not a hard limit
