0ab594ff51847c3d2be09c983ac0f2f36ac5167d
Robert Kaye <rob@musicbrainz.org>
2023-08-11 14:49:48 +0200

    Fix test

--- troi/musicbrainz/recording_lookup.py
+++ troi/musicbrainz/recording_lookup.py
@@ -38,7 +38,7 @@ class RecordingLookupElement(Element):
 
         data = []
         for r in recordings:
-            if r.artist.name == "" or len(r.artist.mbids) == 0 or r.name == "":
+            if r.artist is None or r.artist.name == "" or len(r.artist.mbids) == 0 or r.name == "":
                 data.append({ '[recording_mbid]': r.mbid })
 
         # If we have all the data for all the recordings, no need to lookup anything and simply pass the data along
