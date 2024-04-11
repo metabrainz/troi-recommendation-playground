60af3c6b10f5ca614eb941a82a0ed04aac097e7a
Robert Kaye <rob@musicbrainz.org>
2024-04-11 12:22:27 +0200

    LB radio stats working

--- troi/patches/lb_radio_classes/stats.py
+++ troi/patches/lb_radio_classes/stats.py
@@ -5,7 +5,7 @@ from uuid import UUID
 import troi
 import liblistenbrainz
 import liblistenbrainz.errors
-from troi import Artist, Recording
+from troi import Artist, ArtistCredit, Recording
 from troi import TARGET_NUMBER_OF_RECORDINGS
 from troi.parse_prompt import TIME_RANGES
 
@@ -49,7 +49,8 @@ class LBRadioStatsRecordingElement(troi.Element):
             raise RuntimeError("Cannot fetch recording stats for user %s" % self.user_name)
 
         if result is None or "recordings" not in result["payload"]:
-            raise RuntimeError("There are no stats available for user '%s' for the %s time_range." % (self.user_name, self.time_range))
+            raise RuntimeError("There are no stats available for user '%s' for the %s time_range." %
+                               (self.user_name, self.time_range))
 
         # Give feedback on what we collected
         self.local_storage["data_cache"]["element-descriptions"].append(f"{self.user_name}'s stats for {self.time_range}")
@@ -58,7 +59,9 @@ class LBRadioStatsRecordingElement(troi.Element):
         recordings = []
         for r in result['payload']['recordings']:
             if r['recording_mbid'] is not None:
-                recordings.append(Recording(mbid=r['recording_mbid'], artist=Artist(mbids=r["artist_mbids"])))
+                artists = [Artist(mbid=mbid) for mbid in r["artist_mbids"]]
+                artist_credit = ArtistCredit(artists=artists, musicbrainz={"artist_mbids": r["artist_mbids"]})
+                recordings.append(Recording(mbid=r['recording_mbid'], artist_credit=artist_credit))
 
         # Shuffle the recordings
         shuffle(recordings)
@@ -66,7 +69,8 @@ class LBRadioStatsRecordingElement(troi.Element):
         # Check to make sure we're not going to have tracks by the same artist sequentially
         for i in range(len(recordings), 0, -1):
             try:
-                if recordings[i].artist.mbids == recordings[i+1].artist.mbids:
+                if recordings[i].artist_credit.musicbrainz["artist_mbids"] == recordings[
+                        i + 1].artist_credit.musicbrainz["artist_mbids"]:
                     recordings.pop(i)
             except IndexError:
                 pass
