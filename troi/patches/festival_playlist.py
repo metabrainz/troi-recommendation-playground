36edb79aad552b9fe9e0a47c14b59a9be358dcbb
Robert Kaye <rob@musicbrainz.org>
2024-06-03 13:23:55 +0200

    Starting work on two diff types of playlists

--- troi/patches/festival_playlist.py
+++ troi/patches/festival_playlist.py
@@ -39,12 +39,21 @@ class FestivalPlaylistElement(Element):
             artists = [ Artist(mbid=artist_mbid) ]
             result.append(ArtistCredit(artists=artists))
 
+        self.local_storage["_playlist_name"] = "%s artist discovery playlist" % data["name"]
+        self.local_storage["_playlist_desc"] = "Discover artists that are playing at %s. This playlist " % data["name"] + \
+                                               "contains one track by each of the artists playing at the festival. " +  \
+                                               "Hopefully this playlist allows you to plan your festival better!"
+
         return result
 
 
-class PopularTracksElement(Element):
+class FestivalTracksElement(Element):
     '''
     '''
+
+    def __init__(self, ptype):
+        super().__init__()
+        self.ptype = ptype
 
     @staticmethod
     def inputs():
@@ -54,31 +63,86 @@ class PopularTracksElement(Element):
     def outputs():
         return [Recording]
 
+    def get_album_recording(self, artist_mbid):
+
+        print("Get album track for %s" % artist_mbid)
+        while True:
+            r = requests.get("https://musicbrainz.org/ws/2/artist/%s/?fmt=json&inc=releases" % artist_mbid)
+            if r.status_code == 503:
+                sleep(2)
+                continue
+
+            if r.status_code == 404:
+                print("No albums for %s" % artist_mbid)
+                return None
+
+            if r.status_code != 200:
+                raise PipelineError("Cannot find artist %s: %s" % (artist_mbid, r.text))
+            else:
+                break
+
+        releases = r.json()["releases"]
+        releases = sorted(releases, key=lambda a: a.get("date", "000"), reverse=True)
+
+        try:
+            release_mbid = releases[0]["id"]
+        except IndexError:
+            print("Found no releases for artist %s" % artist_mbid)
+            return None
+
+        while True:
+            r = requests.get("https://musicbrainz.org/ws/2/release/%s/?fmt=json&inc=recordings" % release_mbid)
+            if r.status_code == 503:
+                sleep(2)
+                continue
+
+            if r.status_code == 404:
+                print("Couldn't find release %s" % release_mbid)
+                return None
+
+            if r.status_code != 200:
+                raise PipelineError("Cannot find release %s: %s" % (release_mbid, r.text))
+            else:
+                break
+
+        tracks = plist(r.json()["media"][0]["tracks"])
+        track = tracks.random_item()
+
+        return Recording(mbid=track["recording"]["id"], name=track["title"])
+
+
     def read(self, inputs):
 
         recordings = []
         for artist in inputs[0]:
             artist_mbid = artist.artists[0].mbid
-            while True:
-                r = requests.get("https://api.listenbrainz.org/1/popularity/top-recordings-for-artist/%s" % artist_mbid)
-                if r.status_code == 429:
-                    sleep(2)
-                    continue
+            print("process %s" % artist_mbid)
 
-                if r.status_code == 404:
-                    print("No top tracks for %s" % artist_mbid)
-                    continue
+            if self.ptype == "top":
+                while True:
+                    r = requests.get("https://api.listenbrainz.org/1/popularity/top-recordings-for-artist/%s" % artist_mbid)
+                    if r.status_code == 429:
+                        sleep(2)
+                        continue
 
-                if r.status_code != 200:
-                    raise PipelineError("Cannot find artist %s: %s" % (artist_mbid, r.text))
-                else:
-                    break
+                    if r.status_code == 404:
+                        print("No top tracks for %s" % artist_mbid)
+                        continue
 
-            data = plist(r.json()[:10])
+                    if r.status_code != 200:
+                        raise PipelineError("Cannot find artist %s: %s" % (artist_mbid, r.text))
+                    else:
+                        break
+
+                data = plist(r.json()[:10])
+            else:
+                data = None
+
             if not data:
-                print("No top data for artist: %s" % artist_mbid)
+                rec = self.get_album_recording(artist_mbid)
+                if rec:
+                    recordings.append(rec)
                 continue
-
             
             recording = data.random_item()
             recordings.append(Recording(mbid=recording["recording_mbid"], name=recording["recording_name"]))
@@ -101,9 +165,13 @@ class FestivalPlaylistPatch(Patch):
         Save the current recommended tracks for a given user and type (top or similar).
 
         \b
+        TYPE: The type of festival playlist to generate. Must be "recent" to create a playlist
+              from recent releases by artists or "top" to create a playlist that contains
+              popular tracks for the artists.
         EVENT_MBID: The MBID of the festival event.
         """
         return [
+            {"type": "argument", "args": ["type"]},
             {"type": "argument", "args": ["event_mbid"]}
         ]
 
@@ -120,19 +188,21 @@ class FestivalPlaylistPatch(Patch):
         return "Create a festival playlist from a MusicBrainz event."
 
     def create(self, inputs):
+        ptype = inputs['type']
         event_mbid = inputs['event_mbid']
+
+        if ptype not in ("top", "recent"):
+            raise RuntimeError("Festival playlist type must be either 'top' or 'recent'")
 
         f_playlist = FestivalPlaylistElement(event_mbid)
 
-        popular = PopularTracksElement()
+        popular = FestivalTracksElement(ptype)
         popular.set_sources(f_playlist)
 
         rec_lookup = RecordingLookupElement()
         rec_lookup.set_sources(popular)
 
-        pl_maker = PlaylistMakerElement(name="Festival playlist",
-                                        desc="festival",
-                                        shuffle=True)
+        pl_maker = PlaylistMakerElement(shuffle=True)
         pl_maker.set_sources(rec_lookup)
 
         return pl_maker
