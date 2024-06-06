758181f6891fc332f259c9f58e5e5f04f98a36a6
Robert Kaye <rob@musicbrainz.org>
2024-06-06 12:01:22 +0200

    Festivals is coming along. Slowly.

--- troi/patches/festival_playlist.py
+++ troi/patches/festival_playlist.py
@@ -13,9 +13,10 @@ class FestivalPlaylistElement(Element):
     '''
     '''
 
-    def __init__(self, event_mbid):
+    def __init__(self, event_mbid, event_type):
         super().__init__()
         self.event_mbid = event_mbid
+        self.event_type = event_type
 
     @staticmethod
     def inputs():
@@ -25,24 +26,47 @@ class FestivalPlaylistElement(Element):
     def outputs():
         return [ArtistCredit]
 
-    def read(self, inputs):
-
-        r = requests.get("https://musicbrainz.org/ws/2/event/%s?fmt=json&inc=artist-rels" % self.event_mbid)
+    def load_event(self, event_mbid):
+        r = requests.get("https://musicbrainz.org/ws/2/event/%s?fmt=json&inc=artist-rels+event-rels" % event_mbid)
         if r.status_code != 200:
             raise PipelineError("Cannot find festival %s: %s" % (self.event_mbid, r.text))
 
         data = r.json()
 
-        artist_mbids = [r["artist"]["id"] for r in data["relations"] ]
+        artist_mbids = []
+        event_mbids = []
+
+        for r in data["relations"]:
+            if "artist" in r:
+                artist_mbids.append(r["artist"]["id"])
+            if "event" in r:
+                event_mbids.append(r["event"]["id"])
+
+        return artist_mbids, event_mbids, data["name"]
+
+    def read(self, inputs):
+
+        artist_mbids, event_mbids, name = self.load_event(self.event_mbid)
+        for event_mbid in event_mbids:
+            event_artist_mbids, event_event_mbids, _ = self.load_event(event_mbid)
+            artist_mbids.extend(event_artist_mbids)
+
         result = []
         for artist_mbid in artist_mbids:
             artists = [ Artist(mbid=artist_mbid) ]
             result.append(ArtistCredit(artists=artists))
 
-        self.local_storage["_playlist_name"] = "%s artist discovery playlist" % data["name"]
-        self.local_storage["_playlist_desc"] = "Discover artists that are playing at %s. This playlist " % data["name"] + \
-                                               "contains one track by each of the artists playing at the festival. " +  \
-                                               "Hopefully this playlist allows you to plan your festival better!"
+        desc = "Discover artists that are playing at %s. This playlist contains " % name
+        if self.event_type == "recent":
+            desc += "one recent track "
+        elif self.event_type == "top":
+            desc += "one popular track  "
+        elif self.event_type == "both":
+            desc += "one recent and one popular track "
+        desc += "by each of the artists playing at the festival. Hopefully this playlist allows you to plan your festival better!"
+
+        self.local_storage["_playlist_name"] = "%s artist discovery playlist" % name
+        self.local_storage["_playlist_desc"] = desc
 
         return result
 
@@ -51,9 +75,9 @@ class FestivalTracksElement(Element):
     '''
     '''
 
-    def __init__(self, ptype):
+    def __init__(self, event_type):
         super().__init__()
-        self.ptype = ptype
+        self.event_type = event_type
 
     @staticmethod
     def inputs():
@@ -110,44 +134,46 @@ class FestivalTracksElement(Element):
 
         return Recording(mbid=track["recording"]["id"], name=track["title"])
 
+    def get_top_recording(self, artist_mbid, count=10):
+        while True:
+            r = requests.get("https://api.listenbrainz.org/1/popularity/top-recordings-for-artist/%s" % artist_mbid)
+            if r.status_code == 429:
+                sleep(2)
+                continue
+
+            if r.status_code == 404:
+                print("No top tracks for %s" % artist_mbid)
+                continue
+
+            if r.status_code != 200:
+                raise PipelineError("Cannot find artist %s: %s" % (artist_mbid, r.text))
+            else:
+                break
+
+        recordings = plist(r.json()[:count])
+        if not recordings:
+            return None
+
+        rec = recordings.random_item()
+
+        return Recording(mbid=rec["recording_mbid"], name=rec["recording_name"])
 
     def read(self, inputs):
 
         recordings = []
         for artist in inputs[0]:
             artist_mbid = artist.artists[0].mbid
-            print("process %s" % artist_mbid)
 
-            if self.ptype == "top":
-                while True:
-                    r = requests.get("https://api.listenbrainz.org/1/popularity/top-recordings-for-artist/%s" % artist_mbid)
-                    if r.status_code == 429:
-                        sleep(2)
-                        continue
+            if self.event_type in ("top", "both"):
+                rec = self.get_top_recording(artist_mbid)
+                if rec:
+                    recordings.append(rec)
 
-                    if r.status_code == 404:
-                        print("No top tracks for %s" % artist_mbid)
-                        continue
-
-                    if r.status_code != 200:
-                        raise PipelineError("Cannot find artist %s: %s" % (artist_mbid, r.text))
-                    else:
-                        break
-
-                data = plist(r.json()[:10])
-            else:
-                data = None
-
-            if not data:
+            if self.event_type in ("recent", "both"):
                 rec = self.get_album_recording(artist_mbid)
                 if rec:
                     recordings.append(rec)
-                continue
             
-            recording = data.random_item()
-            recordings.append(Recording(mbid=recording["recording_mbid"], name=recording["recording_name"]))
-            print(recording["artist_name"])
-
         return recordings
 
 
@@ -188,15 +214,15 @@ class FestivalPlaylistPatch(Patch):
         return "Create a festival playlist from a MusicBrainz event."
 
     def create(self, inputs):
-        ptype = inputs['type']
+        event_type = inputs['type']
         event_mbid = inputs['event_mbid']
 
-        if ptype not in ("top", "recent"):
-            raise RuntimeError("Festival playlist type must be either 'top' or 'recent'")
+        if event_type not in ("top", "recent", "both"):
+            raise RuntimeError("Festival playlist type must be either 'top', 'recent' or 'both'")
 
-        f_playlist = FestivalPlaylistElement(event_mbid)
+        f_playlist = FestivalPlaylistElement(event_mbid, event_type)
 
-        popular = FestivalTracksElement(ptype)
+        popular = FestivalTracksElement(event_type)
         popular.set_sources(f_playlist)
 
         rec_lookup = RecordingLookupElement()
