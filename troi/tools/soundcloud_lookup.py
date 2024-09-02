6ad974a045876e5e4bd408d42a6452808cf048ea
Rimma Kubanova <rimma.kubanova@gmail.com>
2024-09-02 13:55:33 +0500

    added logger

--- troi/tools/soundcloud_lookup.py
+++ troi/tools/soundcloud_lookup.py
@@ -131,11 +131,7 @@ def get_tracks_from_soundcloud_playlist(developer_token, playlist_id):
     """ Get tracks from the Soundcloud playlist.
     """
     soundcloud = SoundcloudAPI(developer_token)
-    data = soundcloud.get_playlist_tracks(playlist_id, linked_partitioning=True, limit=100, access=['playable, preview ,blocked'])
-    logger.info(data)
-    tracks = data["tracks"]
-    name = data["title"]
-    description = data["description"]
+    tracks = soundcloud.get_playlist_tracks(playlist_id, linked_partitioning=True, limit=100, access=['playable, preview ,blocked'])
 
     mapped_tracks = [
         {
@@ -145,7 +141,7 @@ def get_tracks_from_soundcloud_playlist(developer_token, playlist_id):
         for track in tracks
     ]
 
-    return mapped_tracks, name, description
+    return mapped_tracks
 
 
 def get_soundcloud_playlist(developer_token, playlist_id):
