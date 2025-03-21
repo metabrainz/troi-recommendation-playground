3c97de9a49928533d3cf9f8df44f02fae3121499
Robert Kaye <rob@musicbrainz.org>
2025-03-21 14:32:31 +0100

    Finish implementing local rate limiter

--- troi/http_request.py
+++ troi/http_request.py
@@ -1,9 +1,12 @@
 import requests
-from time import time
+from time import time, sleep
 from requests.adapters import HTTPAdapter
 from requests.packages.urllib3.util.retry import Retry
+from urllib.parse import urlparse
 
-from icecream import ic
+# Index keep track of rate limits of the various services 
+# we may call. key: scheme, domain value: RateLimit-Limit, Remaining, Reset
+domain_ratelimit_lookup = {}
 
 def requests_retry_session(
     retries=3,
@@ -26,35 +29,53 @@ def requests_retry_session(
 
 
 def http_get(url, headers=None, params=None, **kwargs):
+    return http_fetch(url, "GET", headers=headers, params=params, **kwargs)
+
+def http_post(url, headers=None, params=None, **kwargs):
+    return http_fetch(url, "POST", headers=headers, params=params, **kwargs)
+
+def http_fetch(url, method, headers=None, params=None, **kwargs):
 
     if not headers:
         headers = {}
     headers["User-Agent"] = "ListenBrainz Troi (rob@meb)"
 
+    if method not in ("GET", "POST"):
+        raise ValueError("Only GET and POST are supported.")
+
     session = requests_retry_session()
+    parse = urlparse(url)
     while True:
-        # Switch back to session!
-        r = requests.get(url, params=params, headers=headers, **kwargs)
-        print("X-RateLimit-Limit    ", r.headers["X-RateLimit-Limit"])
-        print("X-RateLimit-Remaining", r.headers["X-RateLimit-Remaining"])
-        print("X-RateLimit-Reset    ", r.headers["X-RateLimit-Reset"])
-        reset = int(r.headers["X-RateLimit-Reset"]) - time()
-        if reset < 0:
-            print("reset time            passed")
+        _key = parse.scheme + parse.netloc
+        if _key in domain_ratelimit_lookup:
+            (limit, remaining, reset) = domain_ratelimit_lookup[_key]
+            time_left = reset - time()
+            if time_left > 0:
+                time_to_wait = time_left / remaining
+                sleep(time_to_wait)
+            del domain_ratelimit_lookup[_key]
+
+        if method == "GET":
+            r = session.get(url, params=params, headers=headers, **kwargs)
         else:
-            print("reset time in         %.2f seconds" % reset)
-        print("response code        ", r.status_code)
+            r = session.post(url, params=params, headers=headers, **kwargs)
+
+        reset = int(r.headers["X-RateLimit-Reset"])
+        remaining = int(r.headers["X-RateLimit-Remaining"])
+        limit = int(r.headers["X-RateLimit-Limit"])
+        domain_ratelimit_lookup[_key] = (limit, remaining, reset)
+
+        # This should never happen, but if it does, just retry
         if r.status_code in (503, 419):
-            ic(r.headers)
-        else:
-            return r
+            continue
 
-print("\nLISTENBRAINZ")
-for i in range(10):
-    resp = http_get("https://api.listenbrainz.org/1/metadata/recording?recording_mbids=e97f805a-ab48-4c52-855e-07049142113d")
-    print()
+        return r
 
-print("\nMUSICBRAINZ")
-for i in range(10):
-    resp = http_get("https://musicbrainz.org/ws/2/artist/8f6bd1e4-fbe1-4f50-aa9b-94c450ec0f11?fmt=json")
-    print()
+if __name__ == "__main__":
+    print("\nLISTENBRAINZ")
+    for i in range(500):
+        resp = http_get("https://api.listenbrainz.org/1/metadata/recording?recording_mbids=e97f805a-ab48-4c52-855e-07049142113d")
+
+    #print("\nMUSICBRAINZ")
+    #for i in range(10):
+    #    resp = http_get("https://musicbrainz.org/ws/2/artist/8f6bd1e4-fbe1-4f50-aa9b-94c450ec0f11?fmt=json")
